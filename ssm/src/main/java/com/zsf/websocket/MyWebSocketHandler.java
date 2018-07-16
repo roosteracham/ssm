399b423b7451aab1dbe3f2cd95c2f21cd9c5e6bd
rooster <384546294@qq.com>
2018-07-16 15:38:10 +0800

    使用核心池大小为1scheduledExecutorService 每秒发送测点数据。

--- ssm/src/main/java/com/zsf/websocket/MyWebSocketHandler.java
+++ ssm/src/main/java/com/zsf/websocket/MyWebSocketHandler.java
@@ -3,6 +3,9 @@ package com.zsf.websocket;
 import org.springframework.web.socket.*;
 
 import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -10,62 +13,59 @@ import java.io.IOException;
  */
 public class MyWebSocketHandler implements WebSocketHandler {
 
-    //定义一个全局的初始化值count=0
-    private static int i = 0;
-
     private Thread thread;
+
+    private static final ScheduledExecutorService executorService =
+            Executors.newSingleThreadScheduledExecutor();
+
+    private String receivedMessage;
+
 
     //建立连接后的操作
     public void afterConnectionEstablished(WebSocketSession session)
             throws Exception {
         System.out.println("已建立连接..");
 
+        executorService.scheduleWithFixedDelay(new Runnable() {
+            @Override
+            public void run() {
+
+                String[] strings = receivedMessage.split(",");
+
+                StringBuilder stringBuilder = new StringBuilder("{");
+
+                for (int j = 0; j < strings.length; j++) {
+                    stringBuilder.append("\"")
+                            .append(strings[j])
+                            .append("\":\"")
+                            .append((int)(Math.random() * 50));
+                    if (j == strings.length - 1) {
+                        stringBuilder.append("\"");
+                    } else {
+                        stringBuilder.append("\",");
+                    }
+                }
+                stringBuilder.append("}");
+
+                try {
+                    session.sendMessage(new TextMessage(stringBuilder.toString()));
+                }catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, 1, 1, TimeUnit.SECONDS);
     }
 
     //消息处理，在客户端通过Websocket API发送的消息会经过这里，然后进行相应的处理
     public void handleMessage(WebSocketSession session,
                               WebSocketMessage<?> message) throws Exception {
-        String receivedMessage = message.getPayload().toString();
-
-        System.out.println(receivedMessage);
-        sendMessages(session, receivedMessage);
-        thread.start();
+        receivedMessage = message.getPayload().toString();
+        //System.out.println(receivedMessage);
+        //sendMessages(session, receivedMessage);
     }
 
     private void sendMessages(WebSocketSession session, String receivedMessage) {
-        thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
 
-                String[] strings = receivedMessage.split(",");
-                while (!"close".equals(receivedMessage)) {
-
-                    StringBuilder stringBuilder = new StringBuilder("{");
-
-                    for (int j = 0; j < strings.length; j++) {
-                        stringBuilder.append("\"")
-                                .append(strings[j])
-                                .append("\":\"")
-                                .append((int)(Math.random() * 50));
-                        if (j == strings.length - 1) {
-                            stringBuilder.append("\"");
-                        } else {
-                            stringBuilder.append("\",");
-                        }
-                    }
-                    stringBuilder.append("}");
-
-                    try {
-                    session.sendMessage(new TextMessage(stringBuilder.toString()));
-                    Thread.sleep(1000);
-                    }catch (IOException e) {
-                        e.printStackTrace();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
     }
 
     //消息传输错误处理
@@ -84,5 +84,33 @@ public class MyWebSocketHandler implements WebSocketHandler {
 
     public boolean supportsPartialMessages() {
         return false;
+    }
+
+    public static void main(String[] args) {
+
+        int[] a = new int[2];
+        executorService.scheduleWithFixedDelay(new Runnable() {
+            @Override
+            public void run() {
+
+                for (int i = 0; i < a.length; i++) {
+                    System.out.print(a[i] + " ");
+                }
+            }
+        }, 1, 1, TimeUnit.SECONDS);
+        try {
+            Thread.sleep(3000);
+        }catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+
+        a[1] = 1;
+
+        executorService.scheduleWithFixedDelay(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("next");
+            }
+        }, 1, 1, TimeUnit.SECONDS);
     }
 }
